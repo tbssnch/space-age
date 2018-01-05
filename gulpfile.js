e903dafffa1410103a86af1b5dd279d8d9d6b4e5
Tyler Bussanich <tbussanich@gmail.com>
2018-01-05 09:33:48 -0800

    install/require gulp uglify and gulp utilities. Create minifyScripts and build task. The minifyScripts task will minimize all scripts. The build task will run minifyScripts if a production build. It will run the jsBrowserify if a dev build.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var utilities = require('gulp-util');
+var buildProduction = utilities.env.production;
 
 gulp.task('concatInterface', function() {
   return gulp.src(['./src/js/*-interface.js'])
@@ -14,4 +17,18 @@ gulp.task('jsBrowserify', ['concatInterface'] function() {
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('minifyScripts', ['jsBrowserify'], function() {
+  return gulp.src('./dist/js/app.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('build', function() {
+  if(buildProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify');
+  }
 });
