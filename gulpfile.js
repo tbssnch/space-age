cfdba3d57acae4250217d4a4777f4c37e39b8238
Tyler Bussanich <tbussanich@gmail.com>
2018-01-05 09:45:36 -0800

    install/require del and jshint. Create a clean task that was added as a dependency to the build task to start with new files. Create jshint task that will allow testing for syntax errors.

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var utilities = require('gulp-util');
 var buildProduction = utilities.env.production;
+var del = require('del');
+var jshint = require('gulp-jshint');
 
 gulp.task('concatInterface', function() {
   return gulp.src(['./src/js/*-interface.js'])
@@ -25,10 +27,20 @@ gulp.task('minifyScripts', ['jsBrowserify'], function() {
     .pipe(gulp.dest('./dist/js'));
 });
 
-gulp.task('build', function() {
+gulp.task('clean', function() {
+  return del(['dist', 'tmp']);
+});
+
+gulp.task('build', ['clean'] function() {
   if(buildProduction) {
     gulp.start('minifyScripts');
   } else {
     gulp.start('jsBrowserify');
   }
+});
+
+gulp.task('jshint', function() {
+  return gulp.src(['src/js/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
